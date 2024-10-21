6ca8a1b8a56760b1d77b05558b1636a1d58277a0
ArpanaM <muriki.arpana@gmail.com>
2024-10-21 22:50:58 +0530

    My first file

--- variables.sh
+++ variables.sh
@@ -4,3 +4,7 @@ echo a=$a
 echo a in currecncy = ${a}USD
 DATE=2024-10-21
 echo good morning, welcome!, today date is ${DATE}
+
+# veriable from shell command line
+echo USER = ${USER}
+echo A = ${A}
