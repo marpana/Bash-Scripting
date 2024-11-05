65e82c8b252f63e1aee557525adadc061d67f807
ArpanaM <muriki.arpana@gmail.com>
2024-11-05 14:57:34 +0530

    cond2 file added

--- cond2.sh
+++ cond2.sh
@@ -6,5 +6,5 @@ if [ ! -z "${age}" -a "${age}" -lt 18 ]; then
   elif [ ! -z "${age}" -a "${age}" -gt 60 ]; then
     echo you are senior
     else
-      yecho you are minor
+      echo you are minor
       fi
