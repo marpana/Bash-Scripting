e83375be11c925552ba3a93d42197f9d35b7b24c
ArpanaM <muriki.arpana@gmail.com>
2024-11-05 14:47:23 +0530

    new file inputs.sh

--- conditions.sh
+++ conditions.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 read -p "enter your age:" age
 
+if [ -z "$age" ]; then
+  echo Input Missing
+  exit
+  fi
 if [ "${age}" -lt 18 ]; then
   echo you are minor
   elif [ "${age}" -gt 60 ]; then
