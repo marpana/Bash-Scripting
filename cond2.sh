e7da587ccbffff907d61202bf4d1097fa16aadca
ArpanaM <muriki.arpana@gmail.com>
2024-11-05 14:53:01 +0530

    cond2 file added

---
+++ cond2.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ ! -z "${age}" -a "${age}" -lt 18 ]; then
+  echo you are minor
+  elif [ ! -z "${age}" -a "${age}" -gt 60 ]; then
+    echo you are senior
+    else
+      yecho you are minor
+      fi
